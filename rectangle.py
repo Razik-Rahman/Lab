28026a410152eae9a06506b3a9b8a1f267219744
Razik-Rahman <razikrahman7@gmail.com>
2025-07-16 21:46:45 +0530

    Delete rectangle.py

--- rectangle.py
+++
@@ -1,8 +0,0 @@
-# Write a program to calculate the area and perimeter of a rectangle given length and breadth
-
-length=int(input("Enter the length"))
-breadth=int(input("Enter the breadth"))
-perimeter=2*(length+breadth)
-area=length*breadth
-print("Perimeter of Rectangle:",perimeter)
-print("Area of Rectangle",area)
