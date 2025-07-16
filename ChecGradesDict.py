98c30d3cc287539f63faaf830137e46b1db48ca7
Razik-Rahman <razikrahman7@gmail.com>
2025-07-16 21:36:59 +0530

    Delete ChecGradesDict.py

--- ChecGradesDict.py
+++
@@ -1,9 +0,0 @@
-s = {}
-n = int(input("Enter the limit "))
-for i in range(n):
-    score = int(input("Enter the score: ")) 
-    name = input("Enter the name: ")             
-    s[score] = name
-for k,v in s.items():
-    print(f"Score: {k}, Name:{v}")
-
