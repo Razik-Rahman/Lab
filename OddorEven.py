ddfa9bdc9da44b61b97e51ff7e3222991c416478
Razik-Rahman <razikrahman7@gmail.com>
2025-07-16 21:45:23 +0530

    Delete OddorEven.py

--- OddorEven.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the number"))
-if n % 2==0:
-    if n==0:
-        print("The number is zero")
-    else:
-        print("The number is even")
-else:
-    print("The number is odd")
-    
